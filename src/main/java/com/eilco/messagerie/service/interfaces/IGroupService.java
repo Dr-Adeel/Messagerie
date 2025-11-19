fea6c7c3d4b512b74a57b592317762b477504590
Kefabd <abdoukefsi@gmail.com>
2025-11-19 13:17:05 +0100

    group innterface

--- src/main/java/com/eilco/messagerie/service/interfaces/IGroupService.java
+++ src/main/java/com/eilco/messagerie/service/interfaces/IGroupService.java
@@ -3,5 +3,9 @@ package com.eilco.messagerie.service.interfaces;
 import com.eilco.messagerie.repositories.entities.Group;
 
 public interface IGroupService {
+    // Method to create a group
+    Group createGroup(Group group);
 
+    // Method to delete a group by its ID
+    void deleteGroup(Long groupId);
 }
