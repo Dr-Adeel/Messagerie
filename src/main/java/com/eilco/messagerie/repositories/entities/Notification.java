39031fba6a15eb480a227947eb7866c213f9eac6
nourrad <nouhailaourrad0@gmail.com>
2025-11-19 14:55:36 +0100

    Response and Request Notification objects

--- src/main/java/com/eilco/messagerie/repositories/entities/Notification.java
+++ src/main/java/com/eilco/messagerie/repositories/entities/Notification.java
@@ -20,6 +20,8 @@ public class Notification {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
+
     @Column(name = "message_id", nullable = false)
     private Long messageId;
 
