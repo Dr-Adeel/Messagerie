24fb5f2205d1d17cfca074be433d954a9fd743e2
oussamajaldi <jaldi.oussam@gmail.com>
2025-11-26 10:37:14 +0100

    auth controller class for login and register

---
+++ src/main/java/com/eilco/messagerie/controllers/AuthController.java
@@ -0,0 +1,36 @@
+package com.eilco.messagerie.controllers;
+
+import com.eilco.messagerie.models.request.UserRequest;
+import com.eilco.messagerie.models.response.UserResponse;
+import com.eilco.messagerie.services.JWTService;
+import com.eilco.messagerie.services.interfaces.IUserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+
+    private final JWTService jwtService;
+    private final IUserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> create(@Valid @RequestBody UserRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.create(request));
+    }
+
+    @PostMapping("/login")
+    public String getToken(Authentication authentication) {
+        return jwtService.generateToken(authentication);
+    }
+
+}
