c6d885d7acd0c6abc58e364f2f1f1aa2b93cc98c
oussamajaldi <jaldi.oussam@gmail.com>
2025-12-13 17:54:29 +0100

    Add missing services for groups and some fix bugs so that the project runs correctly

--- src/main/java/com/eilco/messagerie/services/interfaces/IGroupService.java
+++ src/main/java/com/eilco/messagerie/services/interfaces/IGroupService.java
@@ -2,8 +2,12 @@ package com.eilco.messagerie.services.interfaces;
 
 import com.eilco.messagerie.models.request.GroupRequest;
 import com.eilco.messagerie.models.response.GroupResponse;
+import com.eilco.messagerie.repositories.entities.Group;
+import com.eilco.messagerie.repositories.entities.User;
 
 public interface IGroupService {
+    Group getById(Long id);
+
     GroupResponse createGroup(GroupRequest groupRequest, String creatorUsername);
 
     void addMember(Long groupId, String username);
@@ -11,4 +15,8 @@ public interface IGroupService {
     void removeMember(Long groupId, String username);
 
     GroupResponse getUserGroup(String username);
+
+    void deleteGroup(Long groupId);
+
+    public boolean isAdminOfGroup(User user, Group group);
 }
