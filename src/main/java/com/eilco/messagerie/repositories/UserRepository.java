a7081b68fa90e105062edbc734ca660437b5724d
Spider47 <simobahloul06@gmail.com>
2025-12-13 21:25:04 +0100

    Formatage

--- src/main/java/com/eilco/messagerie/repositories/UserRepository.java
+++ src/main/java/com/eilco/messagerie/repositories/UserRepository.java
@@ -3,7 +3,6 @@ package com.eilco.messagerie.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eilco.messagerie.repositories.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
